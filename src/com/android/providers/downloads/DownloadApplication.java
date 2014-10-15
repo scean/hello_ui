a6027dafe6ecbf0aa21218cd27cd9faa7ffbef5f
hushuanghong <hushuanghong@xunlei.com>
2014-10-15 17:20:19 +0800

    add mipush by hsh

--- src/com/android/providers/downloads/DownloadApplication.java
+++ src/com/android/providers/downloads/DownloadApplication.java
@@ -2,13 +2,18 @@
 package com.android.providers.downloads;
 import miui.external.Application;
 import com.android.providers.downloads.DownloadApplicationDelegate;
+import com.xiaomi.mipush.sdk.MiPushClient;
 public class DownloadApplication extends miui.external.Application {
+    private static final String APP_ID = "2882303761517144351";
+    private static final String APP_KEY = "5991714422351";
     @Override
     public miui.external.ApplicationDelegate onCreateApplicationDelegate() {
         return new DownloadApplicationDelegate();
     }
 	public void initToken()
 	{
+		com.xiaomi.mipush.sdk.Constants.useOfficial();
+		MiPushClient.registerPush(this, APP_ID, APP_KEY);
 		TokenHelper.getInstance().initWithContext(this.getApplicationContext());
 	}
 	public void uninitToken()
