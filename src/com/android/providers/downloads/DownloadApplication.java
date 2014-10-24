ceba9333cb5c5f90a24304bdc9f05b3049679573
wangsf <wang123sf@126.com>
2014-10-24 20:19:24 +0800

    修复了退出账户仍能收到论坛推送下载的bug

--- src/com/android/providers/downloads/DownloadApplication.java
+++ src/com/android/providers/downloads/DownloadApplication.java
@@ -1,11 +1,12 @@
-
 package com.android.providers.downloads;
+
 import miui.external.Application;
 import com.android.providers.downloads.DownloadApplicationDelegate;
 import com.xiaomi.mipush.sdk.MiPushClient;
+
 public class DownloadApplication extends miui.external.Application {
-    private static final String APP_ID = "2882303761517144351";
-    private static final String APP_KEY = "5991714422351";
+    public static final String APP_ID = "2882303761517144351";
+    public static final String APP_KEY = "5991714422351";
     @Override
     public miui.external.ApplicationDelegate onCreateApplicationDelegate() {
         return new DownloadApplicationDelegate();
