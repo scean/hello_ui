71ae818d27e2ff35278e3afc5cc8e75320347113
Tinker S <sunting.bcwl@gmail.com>
2014-11-05 18:01:12 +0800

    Sometimes createPackageContext can throw 'package not found exception', this is extreamly strange. We should ensure preference not null.

--- ui/src/com/android/providers/downloads/ui/notification/PreferenceLogic.java
+++ ui/src/com/android/providers/downloads/ui/notification/PreferenceLogic.java
@@ -45,7 +45,7 @@ public class PreferenceLogic {
 
     /**赠送流量提醒**/
     private final String GIVEN_FLOW = "given_flow";
-    
+
     private PreferenceLogic(Context context) {
         mContext = context;
     }
@@ -62,7 +62,7 @@ public class PreferenceLogic {
     @SuppressLint("WorldWriteableFiles")
     private SharedPreferences getSharedPreference() {
         if (null == mSharedPreferences) {
-             Context ct = null;
+            Context ct = null;
             try {
                 ct = mContext.createPackageContext(DownloadList.DOWNLOADPROVIDER_PKG_NAME, Context.CONTEXT_IGNORE_SECURITY);
                 mSharedPreferences = ct.getSharedPreferences(
@@ -73,107 +73,97 @@ public class PreferenceLogic {
         return mSharedPreferences;
     }
 
-	private SharedPreferences getNotiSharedPreference() {
+    private SharedPreferences getNotiSharedPreference() {
         if (null == mSharedPreferences) {
-			Context ct = null;
-			try {
-				ct = mContext.createPackageContext(
-						DownloadList.DOWNLOADPROVIDER_PKG_NAME,
-						Context.CONTEXT_IGNORE_SECURITY);
+            Context ct = null;
+            try {
+                ct = mContext.createPackageContext(
+                        DownloadList.DOWNLOADPROVIDER_PKG_NAME,
+                        Context.CONTEXT_IGNORE_SECURITY);
                 mSharedPreferences = ct.getSharedPreferences("sp_notification",
-						Context.MODE_PRIVATE);
-			} catch (Exception e) {
-			}
-		}
+                        Context.MODE_PRIVATE);
+            } catch (Exception e) {
+            }
+        }
         return mSharedPreferences;
-	}
+    }
 
     private void saveStringPre(String key, String value) {
         SharedPreferences mPreferces = getSharedPreference();
-		if (mPreferces != null) {
-			mPreferces.edit().putString(key, value).commit();
-		}
-
+        if (mPreferces != null) {
+            mPreferces.edit().putString(key, value).commit();
+        }
     }
 
     private String getStringPre(String key) {
         SharedPreferences mPreferces = getSharedPreference();
-		if (mPreferces != null) {
-			return mPreferces.getString(key, "");
-		}
-		return "";
+        if (mPreferces != null) {
+            return mPreferces.getString(key, "");
+        }
+        return "";
     }
 
     private void saveBooleanPre(String key, boolean value) {
         SharedPreferences mPreferces = getSharedPreference();
-		if (mPreferces != null) {
-			mPreferces.edit().putBoolean(key, value).commit();
-		}
-
+        if (mPreferces != null) {
+            mPreferces.edit().putBoolean(key, value).commit();
+        }
     }
 
     private boolean getBooleanPre(String key) {
         SharedPreferences mPreferces = getSharedPreference();
-		if (mPreferces == null) {
-			return true;
-		} else {
-			return mPreferces.getBoolean(key, false);
-		}
-
+        if (mPreferces == null) {
+            return true;
+        } else {
+            return mPreferces.getBoolean(key, false);
+        }
     }
 
-	/***
-	 * 
-	 * 保存通知本地弹出信息
-	 * 
-	 * @param key
-	 * @param value
-	 */
-	private void saveNotiBoolean(String key, boolean value) {
+    /***
+     * 保存通知本地弹出信息
+     *
+     * @param key
+     * @param value
+     */
+    private void saveNotiBoolean(String key, boolean value) {
         SharedPreferences mPreferces = getSharedPreference();
-		if (mPreferces != null) {
+        if (mPreferces != null) {
             mPreferces.edit().putBoolean(key, value).commit();
-		}
-	}
+        }
+    }
 
-	/**
-	 * 获得通知本地弹出信息
-	 * 
-	 * @param key
-	 * @return
-	 */
-	private boolean getNotiBoolean(String key) {
+    /**
+     * 获得通知本地弹出信息
+     *
+     * @param key
+     * @return
+     */
+    private boolean getNotiBoolean(String key) {
         SharedPreferences mPreferces = getSharedPreference();
-		if (mPreferces == null) {
-			return true;
-		} else {
+        if (mPreferces == null) {
+            return true;
+        } else {
             return mPreferces.getBoolean(key, false);
-		}
-	}
+        }
+    }
 
     private void saveIntPre(String key, int value) {
         SharedPreferences mPreferces = getSharedPreference();
-		if (mPreferces != null) {
-			mPreferces.edit().putInt(key, value).commit();
-		}
-
+        if (mPreferces != null) {
+            mPreferces.edit().putInt(key, value).commit();
+        }
     }
 
     /***
      * 清楚本地数据弹出记录
-     * 
+     *
      * @param context
      */
-    public static void initNotiSetting(Context context)
-    {
+    public static void initNotiSetting(Context context) {
         PreferenceLogic preferenceLogic = PreferenceLogic.getInstance(context);
-
         preferenceLogic.saveVipExpireTodayIsTip(false);
-
         preferenceLogic.saveBeforeGivenFlowOut("NO_FLOW", false);
-
         preferenceLogic.saveStageOneIsTip(false);
-
         Calendar c = Calendar.getInstance();
         int month = c.get(Calendar.MONTH);
         preferenceLogic.saveGivenFlow(month, false);
@@ -181,23 +171,30 @@ public class PreferenceLogic {
 
     private int getIntPre(String key) {
         SharedPreferences mPreferces = getSharedPreference();
-        return mPreferces.getInt(key, 0);
+        if (mPreferces != null) {
+            return mPreferces.getInt(key, 0);
+        }
+        return 0;
     }
 
     private void saveLongPre(String key, long value) {
         SharedPreferences mPreferces = getSharedPreference();
-        mPreferces.edit().putLong(key, value).commit();
+        if (mPreferces != null) {
+            mPreferces.edit().putLong(key, value).commit();
+        }
     }
 
     private long getLongPre(String key) {
         SharedPreferences mPreferces = getSharedPreference();
-        return mPreferces.getLong(key, 0);
+        if (mPreferces != null) {
+            return mPreferces.getLong(key, 0);
+        }
+        return 0;
     }
 
     public String getToken() {
         return XLUtil.getStringPackagePreference(mContext);
     }
-
 
     /**
      * Save remind date
@@ -240,14 +237,14 @@ public class PreferenceLogic {
         if (null == saveValue || !date.equals(saveValue)) {
             saveStringPre(CURRENT_TODAY, date);
             resetDownloadTimesForToday();
-//            saveStageOneIsTip(false);
-//            saveStageTwoIsTip(false);
+            //            saveStageOneIsTip(false);
+            //            saveStageTwoIsTip(false);
             // 初始化流量使用
-//            List<Integer> beforeUsedPercents = ConfigJSInstance.getInstance(mContext).getConfigJSInfo().flow_guide_bar_before_used_percent_config;
-//            saveBeforeGivenFlowOut(0, false);
-//            for (Integer percent : beforeUsedPercents) {
-//                saveBeforeGivenFlowOut(percent, false);
-//            }
+            //            List<Integer> beforeUsedPercents = ConfigJSInstance.getInstance(mContext).getConfigJSInfo().flow_guide_bar_before_used_percent_config;
+            //            saveBeforeGivenFlowOut(0, false);
+            //            for (Integer percent : beforeUsedPercents) {
+            //                saveBeforeGivenFlowOut(percent, false);
+            //            }
         }
     }
 
@@ -272,11 +269,11 @@ public class PreferenceLogic {
 
     // 保存当天场景一提示
     public void saveStageOneIsTip(boolean isHaveTip) {
-		saveNotiBoolean(STAGE_ONE_IS_TIP, isHaveTip);
+        saveNotiBoolean(STAGE_ONE_IS_TIP, isHaveTip);
     }
 
     public boolean getStageOneIsTip() {
-		return getNotiBoolean(STAGE_ONE_IS_TIP);
+        return getNotiBoolean(STAGE_ONE_IS_TIP);
     }
 
     // 保存当天场景二提示
@@ -289,35 +286,38 @@ public class PreferenceLogic {
     }
 
     public void saveBeforeGivenFlowOut(String ext, boolean hasShown) {
-		saveNotiBoolean(BEFORE_GIVEN_FLOW_OUT + ext, hasShown);
-    } 
-    public boolean isGivenFlowShown(int ext) {
-		return getNotiBoolean(GIVEN_FLOW + ext);
+        saveNotiBoolean(BEFORE_GIVEN_FLOW_OUT + ext, hasShown);
     }
-    
+    public boolean isGivenFlowShown(int ext) {
+        return getNotiBoolean(GIVEN_FLOW + ext);
+    }
+
     public void saveGivenFlow(int ext, boolean hasShown) {
-		saveNotiBoolean(GIVEN_FLOW + ext, hasShown);
+        saveNotiBoolean(GIVEN_FLOW + ext, hasShown);
     }
 
     public boolean isBeforeGivenFlowOutShown(String ext) {
-		return getNotiBoolean(BEFORE_GIVEN_FLOW_OUT + ext);
+        return getNotiBoolean(BEFORE_GIVEN_FLOW_OUT + ext);
     }
+
     public void saveBeforeGivenFlowDay(int day) {
         saveIntPre(BEFORE_GIVEN_FLOW_OUT , day);
     }
+
     public int getBeforeGivenFlowDay() {
         return getIntPre(BEFORE_GIVEN_FLOW_OUT);
     }
 
     public void saveVipExpireOneIsTip(boolean isHaveTip) {
-		saveNotiBoolean(VIP_OUTOF_DATE_ONE, isHaveTip);
+        saveNotiBoolean(VIP_OUTOF_DATE_ONE, isHaveTip);
     }
 
     public boolean getVipExpireOneIsTip() {
-		return getNotiBoolean(VIP_OUTOF_DATE_ONE);
+        return getNotiBoolean(VIP_OUTOF_DATE_ONE);
     }
+
     public void saveVipExpireTodayIsTip(boolean isHaveTip) {
-		saveNotiBoolean(VIP_OUTOF_DATE_TODAY, isHaveTip);
+        saveNotiBoolean(VIP_OUTOF_DATE_TODAY, isHaveTip);
     }
 
     public boolean getVipExpireTodayIsTip() {
@@ -348,41 +348,51 @@ public class PreferenceLogic {
         return getBooleanPre(VIP_OUTOF_DATE);
     }
 
-    
     public void setIsHaveUseXunleiDownload(boolean value){
-    	saveBooleanPre(PREF_KEY_XUNLEI_USAGE_PERMISSION, value);
+        saveBooleanPre(PREF_KEY_XUNLEI_USAGE_PERMISSION, value);
     }
-    
+
     // 是否使用迅雷下载引擎
     public boolean getIsHaveUseXunleiDownload() {
-        if (miui.os.Build.IS_CTS_BUILD || miui.os.Build.IS_INTERNATIONAL_BUILD)
-		//if(true)
-		{
+        if (miui.os.Build.IS_CTS_BUILD || miui.os.Build.IS_INTERNATIONAL_BUILD) {
             return false;
         } else {
             SharedPreferences mPreferces = getSharedPreference();
-            return mPreferces.getBoolean(PREF_KEY_XUNLEI_USAGE_PERMISSION, true);
+            if (mPreferces != null) {
+                return mPreferces.getBoolean(PREF_KEY_XUNLEI_USAGE_PERMISSION, true);
+            }
+            return true;
         }
     }
 
     public void setFirstReceive(boolean isFirst) {
         SharedPreferences mPreferces = getSharedPreference();
-        mPreferces.edit().putBoolean(IS_FIRST_GET, isFirst).commit();
+        if (mPreferces != null) {
+            mPreferces.edit().putBoolean(IS_FIRST_GET, isFirst).commit();
+        }
     }
 
     public boolean getFirstReceive() {
         SharedPreferences mPreferces = getSharedPreference();
-        return mPreferces.getBoolean(IS_FIRST_GET, true);
+        if (mPreferces != null) {
+            return mPreferces.getBoolean(IS_FIRST_GET, true);
+        }
+        return true;
     }
 
     public void setShowNotification(boolean isFirst) {
         SharedPreferences mPreferces = getSharedPreference();
-        mPreferces.edit().putBoolean(SHOW_NOTIFICATION, isFirst).commit();
+        if (mPreferces != null) {
+            mPreferces.edit().putBoolean(SHOW_NOTIFICATION, isFirst).commit();
+        }
     }
 
     public boolean getShowNotification() {
         SharedPreferences mPreferces = getSharedPreference();
-        return mPreferces.getBoolean(SHOW_NOTIFICATION, true);
+        if (mPreferces != null) {
+            return mPreferces.getBoolean(SHOW_NOTIFICATION, true);
+        }
+        return true;
     }
 
     private final String IS_FIRST_GET = "is_first_get";
